12b0cd368fee69ab231945c1f62a3cd04df4fb8c
Vanessa Nguyen <62510025+vanessavy@users.noreply.github.com>
2024-01-28 22:52:39 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
+    },
+    configureWebpack:{
+        performance: {
+          hints: false
+        },
+        optimization: {
+          splitChunks: {
+            minSize: 10000,
+            maxSize: 250000,
+          }
+        }
     }
 }
+
